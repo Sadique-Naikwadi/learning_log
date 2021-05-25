7fbabb151f50b2ef92cc82d7245cb6a4740c21be
Sadique Naikwadi <sadique.techie@gmail.com>
2021-05-25 13:02:48 +0530

    Add app to the settings.py file and make migrations to topic

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'learning_logs.apps.LearningLogsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
