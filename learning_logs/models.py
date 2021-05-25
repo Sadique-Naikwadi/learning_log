398a1be878bf118c1918ac64c1ec0f1c4bf20cb8
Sadique Naikwadi <sadique.techie@gmail.com>
2021-05-25 12:40:02 +0530

    Create Model 'Topic'.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """ A topic user is learning about"""
+    title = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self):
+        """ string presentation of topic"""
+        return self.title
+
+
+    
+
