42c6a5ebcffc42ff3b03ebefa36076b32d366cf2
Sadique Naikwadi <sadique.techie@gmail.com>
2021-06-14 10:52:44 +0530

    Removed Procfile, requirements.txt and made few changes in setting.py file.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['sadique82.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
